181d3f8cefbadf1b1f268c2089bda04da0923425
PiottiTech <peter.piotti@gmail.com>
2019-03-10 11:00:05 -0400

    Initial Commit

---
+++ src/PiottiTech.Api/BaseApiController.cs
@@ -0,0 +1,53 @@
+﻿using PiottiTech.Common;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PiottiTech.Api
+{
+    public class BaseApiController : ApiController
+    {
+        protected HttpResponseMessage ProcessedRequest<T>(T obj)
+        {
+            return Request.CreateResponse<T>(HttpStatusCode.OK, obj, "application/json");
+        }
+
+        #region ProcessedRequestLogException - free logging, automattically news up ApiResult, before calling ProcessedRequest
+
+        protected HttpResponseMessage ProcessedRequestLogException(Exception ex, string message, Object obj)
+        {
+            ApiResult apiResult = new ApiResult(message, ex);
+            return ProcessedRequest(apiResult);
+        }
+
+        protected HttpResponseMessage ProcessedRequestLogException(Exception ex, string message)
+        {
+            ApiResult apiResult = new ApiResult(message, ex);
+            return ProcessedRequest(apiResult);
+        }
+
+        protected HttpResponseMessage ProcessedRequestLogException(Exception ex, Object obj)
+        {
+            ApiResult apiResult = new ApiResult(ex);
+            return ProcessedRequest(apiResult);
+        }
+
+        protected HttpResponseMessage ProcessedRequestLogException(Exception ex)
+        {
+            ApiResult apiResult = new ApiResult(ex);
+            return ProcessedRequest(apiResult);
+        }
+
+        protected HttpResponseMessage ProcessedRequestNoPostBody(Object obj)
+        {
+            string mssg = "POST body is empty. Request could not be processed.";
+            Logger.Log(mssg + obj.ToString());
+            ApiResult apiResult = new ApiResult(false, mssg);
+            return ProcessedRequest(apiResult);
+        }
+
+        #endregion ProcessedRequestLogException - free logging, automattically news up ApiResult, before calling ProcessedRequest
+    }
+}
+
---
+++ src/PiottiTech.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PiottiTech.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PiottiTech Corp")]
+[assembly: AssemblyProduct("PiottiTech.Api")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("83e67832-8762-4fb6-a3b6-e08df6a1c699")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
